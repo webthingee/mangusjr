e2e01a71ad01bcbd5ac2cb4f9f7f0f0d78018ee9
Sean Lange <seanlange@SRLMacBook-558.local>
2017-09-23 16:31:15 -0700

    new spawner

---
+++ Assets/SpawnEnemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour {
+
+	public GameObject enemyPrefab;
+    public GameObject spawnPoint1;
+    public GameObject spawnPoint2;
+
+    void Start () 
+	{
+		StartCoroutine(SpawnEnemyDude(20f));
+	}
+	
+	IEnumerator SpawnEnemyDude (float _waitTime)
+	{
+		GameObject enemy = (GameObject) Instantiate(enemyPrefab, spawnPoint1.transform.position, Quaternion.identity);
+		enemy.GetComponent<CharacterAttr>().CharHealthChange = 10;
+        enemy.GetComponent<CharacterAttr>().CharName = "wannagetid";
+
+		yield return new WaitForSeconds(_waitTime);
+		StartCoroutine(SpawnEnemyDude(20f));
+    }
+}
