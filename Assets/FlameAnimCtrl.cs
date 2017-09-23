f250f57154581a274cc8994fe1439a519d409c17
Sean Lange <seanlange@SRLMacBook-558.local>
2017-09-23 09:20:00 -0700

    we have got fire

--- Assets/FlameAnimCtrl.cs
+++ Assets/FlameAnimCtrl.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class FlameAnimCtrl : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    Animator anim;
+	[SerializeField] float flameOffset;
+	[SerializeField] float flameSpeed;
+    
+    void Awake () 
+	{
+        anim = GetComponent<Animator>();
+		flameOffset = Random.Range(0f, 1.5f);
+		flameSpeed = Random.Range(.5f, 1.5f);
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+		anim.SetFloat("offsetFlame", flameOffset);
+		anim.speed = flameSpeed;
 	}
 }
